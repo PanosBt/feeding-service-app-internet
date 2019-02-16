9c87b2c646cd1c063085b4ea586c60ec0ffa2e00
ShadowGlimpse <allthegamesilove@gmail.com>
2019-02-16 21:59:41 +0200

    fixeda mistake

--- app.js
+++ app.js
@@ -170,7 +170,7 @@ app.post('/updatestudent/:id', (req, res) => {
     };
 
     let options = {
-        hostname: '83.212.102.58g',
+        hostname: '83.212.102.58',
         port: 8080,
         path: '/api/students/' + req.params.id,
         method: 'PATCH',
